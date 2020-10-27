6d561de9c7797a782069411f94319a1bf08f7a11
woongs <ccw1021@naver.com>
2020-10-27 13:57:41 +0900

    [iOS] feat: Label collectionView 기본 설정
    - data source, delegate 연결
    - cell size 설정

--- iOS/IssueTracker/IssueTracker/ViewControllers/LabelViewController.swift
+++ iOS/IssueTracker/IssueTracker/ViewControllers/LabelViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class LabelViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
+// MARK: - UICollectionView Data Source
+
+extension LabelViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         
+        return cell
+    }
+}
+
+// MARK: - UICollectionView Delegate FlowLayout
+
+extension LabelViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.bounds.width, height: 100)
     }
 }
