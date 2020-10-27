0ce41c46b93ed1dadf2f367e250e34a7e87a11b4
skid901 <tlrudejr1993@naver.com>
2020-10-27 18:01:37 +0900

    [server] feat: sequelize seeder 설정
    - 모델 초기 데이터 추가
    
    Close #20

---
+++ web/server/src/db/seeders/20201027083954-user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return queryInterface.bulkInsert('users', [
+      {
+        id: 'test',
+        password: 'test',
+        name: 'Test',
+      },
+    ]);
+  },
+
+  down: async (queryInterface) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('users');
+  },
+};
---
+++ web/server/src/db/seeders/20201027084123-label.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return queryInterface.bulkInsert('labels', [
+      {
+        name: 'bug',
+        color: 'ff0066',
+        description: '버그 수정',
+      },
+      {
+        name: 'documentation',
+        color: '0000ff',
+        description: '문서 작성',
+      },
+    ]);
+  },
+
+  down: async (queryInterface) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('labels');
+  },
+};
