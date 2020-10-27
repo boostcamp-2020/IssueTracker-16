416f585659ebba9a2e72090c0d635fd2119c0f4d
ahrimy <dlsdjdkfla@gmail.com>
2020-10-27 11:18:22 +0900

    [server] chore: sequelize 초기 세팅
    - .sequelizerc: sequelize 설정 파일
    - sequelize: init
    - dotenv 설치

---
+++ web/server/src/db/config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
