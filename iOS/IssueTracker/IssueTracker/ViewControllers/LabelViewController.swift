848539f6f6cc2013f9463c0daf5604e3e18ea410
kyungpyoda <qorghdl@naver.com>
2020-10-28 02:45:07 +0900

    [iOS] feat: 레이블 셀 터치 이벤트 구현
    - 터치 시 편집을 위함.
    - 레이블의 정보를 가지고 prepare에서 inputView를 추가하도록 함.

--- iOS/IssueTracker/IssueTracker/ViewControllers/LabelViewController.swift
+++ iOS/IssueTracker/IssueTracker/ViewControllers/LabelViewController.swift
@@ -15,6 +15,16 @@ class LabelViewController: UIViewController {
         super.viewDidLoad()
         
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let vc = segue.destination as? AddAlertViewController else { return }
+        if let sender = sender as? (String, String, String) {
+            vc.addInputView(title: "제목", placeholder: "", text: sender.0)
+            vc.addInputView(title: "설명", placeholder: "", text: sender.1)
+            vc.addInputView(title: "색상", placeholder: "", text: sender.2)
+        }
+    }
+    
 }
 
 // MARK: - UICollectionView Data Source
@@ -36,5 +46,20 @@ extension LabelViewController: UICollectionViewDataSource {
 extension LabelViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return .init(width: view.bounds.width, height: 80)
+    }
+}
+
+// MARK: - UICollectionView Delegate
+
+extension LabelViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LabelCollectionViewCell.identifier, for: indexPath) as! LabelCollectionViewCell
+        if let labelName = cell.labelName.titleLabel?.text,
+           let labelDescription = cell.labelDescription.text,
+           let labelColor = cell.labelName.backgroundColor?.hexString {
+            performSegue(withIdentifier: "presentAddAlertViewContoller", sender: (labelName, labelDescription, labelColor))
+        } else {
+            performSegue(withIdentifier: "presentAddAlertViewContoller", sender: nil)
+        }
     }
 }
