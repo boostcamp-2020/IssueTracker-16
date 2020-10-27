f4e0d8ef0d26f91e6fb4041ccc36d927f1d38ba5
woongs <ccw1021@naver.com>
2020-10-27 17:09:23 +0900

    [iOS] feat: 추가화면에 기본 뷰 추가
    - 저장, 종료, 초기화 버튼
    - 버튼 IBAction 추가

---
+++ iOS/IssueTracker/IssueTracker/ViewControllers/AddAlertViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddAlertViewController.swift
+//  IssueTracker
+//
+//  Created by woong on 2020/10/27.
+//
+
+import UIKit
+
+class AddAlertViewController: UIViewController {
+    
+    // MARK: - Views
+    
+    @IBOutlet weak private var contentStackView: UIStackView!
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction func touchedCancelButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func touchedAddButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func touchedClearButton(_ sender: UIButton) {
+        
+    }
+}
