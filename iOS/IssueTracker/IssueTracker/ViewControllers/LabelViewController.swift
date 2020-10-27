61db62429822b2de0648bfc03340dfe1505424a6
woongs <ccw1021@naver.com>
2020-10-27 12:35:29 +0900

    [iOS] feat: LabelViewController 생성
    - Navigation embed
    - Large Title 적용

---
+++ iOS/IssueTracker/IssueTracker/ViewControllers/LabelViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LabelViewController.swift
+//  IssueTracker
+//
+//  Created by woong on 2020/10/27.
+//
+
+import UIKit
+
+class LabelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
