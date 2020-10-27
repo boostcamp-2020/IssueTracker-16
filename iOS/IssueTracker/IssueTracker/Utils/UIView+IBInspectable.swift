1d3b09a74d8382a4e2e3d98bf908da86e4c9e9d7
woongs <ccw1021@naver.com>
2020-10-27 20:53:27 +0900

    [iOS] style: IBInspectable UIView 익스텐션으로 이동

---
+++ iOS/IssueTracker/IssueTracker/Utils/UIView+IBInspectable.swift
@@ -0,0 +1,41 @@
+//
+//  UIView+IBInspectable.swift
+//  IssueTracker
+//
+//  Created by woong on 2020/10/27.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
+
+
