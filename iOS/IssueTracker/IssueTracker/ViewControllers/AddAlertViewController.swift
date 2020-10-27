8e7efad530b6411a334cb5e2f6ae91360e44aaaf
woongs <ccw1021@naver.com>
2020-10-27 20:18:10 +0900

    [iOS] feat: addAlertVC Delegate 추가
    - save 버튼 클릭시 [InputView] 리턴
    - cancel 버튼 클릭시

--- iOS/IssueTracker/IssueTracker/ViewControllers/AddAlertViewController.swift
+++ iOS/IssueTracker/IssueTracker/ViewControllers/AddAlertViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
+protocol AddAlertViewControllerDelegate: class {
+    func addAlertViewController<InputView>(_ addAlertViewController: AddAlertViewController, didTabAddWithTextFields: [InputView])
+    
+    func addAlertViewControllerDidCancel(_ addAlertViewController: AddAlertViewController)
+}
+
 class AddAlertViewController: UIViewController {
+    
+    typealias InputView = UITextField
     
     // MARK: - Views
     
     @IBOutlet weak private var contentStackView: UIStackView!
+    
+    // MARK: - Properties
+    
+    weak var delegate: AddAlertViewControllerDelegate?
+    private var inputViews: [InputView] {
+        return contentStackView.arrangedSubviews.compactMap { $0 as? InputView }
+    }
     
     // MARK: - View Life Cycle
     
@@ -19,17 +34,35 @@ class AddAlertViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    // MARK: - Methods
+    
+    func addInputView(title: String, placeholder: String) {
+        let inputView = InputView()
+        inputView.text = title
+        inputView.placeholder = placeholder
+        contentStackView.addArrangedSubview(inputView)
+    }
+    
+    func addInputView(_ inputView: InputView) {
+        
+    }
+    
+    // MARK: Private
+    
     // MARK: IBActions
     
-    @IBAction func touchedCancelButton(_ sender: UIButton) {
-        
+    @IBAction private func touchedCancelButton(_ sender: UIButton) {
+        dismiss(animated: true) { [weak self] in
+            guard let self = self else { return }
+            self.delegate?.addAlertViewControllerDidCancel(self)
+        }
     }
     
-    @IBAction func touchedAddButton(_ sender: UIButton) {
-        
+    @IBAction private func touchedAddButton(_ sender: UIButton) {
+        delegate?.addAlertViewController(self, didTabAddWithTextFields: inputViews)
     }
     
-    @IBAction func touchedClearButton(_ sender: UIButton) {
-        
+    @IBAction private func touchedClearButton(_ sender: UIButton) {
+        inputViews.forEach { $0.text = "" }
     }
 }
