d2cf3f29de279d6d6421a5d51a1a79270f6375ac
kyungpyoda <qorghdl@naver.com>
2020-10-27 11:26:55 +0900

    [iOS] feat: 탭바 설정

---
+++ iOS/IssueTracker/IssueTracker/ViewControllers/IssueViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by 홍경표 on 2020/10/27.
+//
+
+import UIKit
+
+class IssueViewController: UIViewController {
+    
+    // MARK: - Constants
+    
+    // MARK: - Properties
+    
+    // MARK: - Views
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Initialize
+
+    // MARK: - Methods
+    
+    // MARK: Selectors
+    
+    // MARK: IBActions
+    
+}
+
