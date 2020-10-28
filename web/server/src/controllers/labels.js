570e4d9cba23a929c42f0064439ba6552f81150b
ahrimy <dlsdjdkfla@gmail.com>
2020-10-28 20:17:20 +0900

    [server] feat: labels delete API
    - delete API 기능을 위한 service, controller 구현
    - delete된 data가 있으면 200 code로 응답
    - delete된 data가 없으면 404 code로 응답

--- web/server/src/controllers/labels.js
+++ web/server/src/controllers/labels.js
@@ -17,7 +17,15 @@ class LabelController {
 
   // update(req, res) { }
 
-  // delete(req, res) { }
+  async delete(req, res) {
+    const { num } = req.params;
+    const deleted = await labelService.remove({ num });
+    if (deleted) {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(404).json({ success: false });
+    }
+  }
 }
 
 module.exports = new LabelController({ labelService });
