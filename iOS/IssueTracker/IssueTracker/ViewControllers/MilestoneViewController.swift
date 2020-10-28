c680f6d9ff6c220ce97546721e2b668127f7e789
kyungpyoda <qorghdl@naver.com>
2020-10-28 12:56:31 +0900

    [iOS] feat: MilestoneViewController 생성

---
+++ iOS/IssueTracker/IssueTracker/ViewControllers/MilestoneViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MilestoneViewController.swift
+//  IssueTracker
+//
+//  Created by 홍경표 on 2020/10/28.
+//
+
+import UIKit
+
+class MilestoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
