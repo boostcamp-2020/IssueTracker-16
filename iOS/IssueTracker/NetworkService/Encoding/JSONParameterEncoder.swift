7bf4c4a24c1a7c2c034c72022d94d79ca8cab6c2
woongs <ccw1021@naver.com>
2020-10-28 13:40:41 +0900

    [iOS] feat: NetworkService 프레임워크 추가

---
+++ iOS/IssueTracker/NetworkService/Encoding/JSONParameterEncoder.swift
@@ -0,0 +1,23 @@
+//
+//  JSONParameterEncoder.swift
+//  NetworkService
+//
+//  Created by woong on 2020/10/28.
+//
+
+import Foundation
+
+public struct JSONParameterEncoder {
+    public func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        do {
+            let jsonAsData = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+                        
+            urlRequest.httpBody = jsonAsData
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+        } catch {
+            throw NetworkError.encodingFailed
+        }
+    }
+}
---
+++ iOS/IssueTracker/NetworkService/Encoding/URLParametrEncoder.swift
@@ -0,0 +1,33 @@
+//
+//  URLParametrEncoder.swift
+//  NetworkService
+//
+//  Created by woong on 2020/10/28.
+//
+
+import Foundation
+
+public struct URLParameterEncoder: ParameterEncoder {
+    public func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        
+        guard let url = urlRequest.url else { throw NetworkError.missingURL }
+        
+        if var urlComponents = URLComponents(url: url,
+                                             resolvingAgainstBaseURL: false), !parameters.isEmpty {
+            
+            urlComponents.queryItems = [URLQueryItem]()
+            
+            for (key,value) in parameters {
+                let queryItem = URLQueryItem(name: key,
+                                             value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
+                urlComponents.queryItems?.append(queryItem)
+            }
+            urlRequest.url = urlComponents.url
+        }
+        
+        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        }
+        
+    }
+}
