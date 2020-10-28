794b2ff07e7447d99679b3111050b17a6292b646
skid901 <tlrudejr1993@naver.com>
2020-10-29 01:24:02 +0900

    [server] feat: labels update api 추가
    - label router에 put method 추가
    - LabelContoller.update() 메소드 구현
    - LabelService.update() 메소드 구현

--- web/server/src/routes/labels.js
+++ web/server/src/routes/labels.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/labels');
 // router.post(controller.add);
 router.get('', controller.getAll);
 router.get('/:num', controller.getOne);
-// router.put('/:num', controller.update);
+router.put('/:num', controller.update);
 router.delete('/:num', controller.delete);
 
 module.exports = router;
