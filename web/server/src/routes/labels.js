c20071c87c647e53fbfb1cfef10bd248c4efc71f
ahrimy <dlsdjdkfla@gmail.com>
2020-10-29 18:43:58 +0900

    [server] feat: labels 조회 API error handler 적용
    - labels 전체조회 API의 controller에 error handler 적용

--- web/server/src/routes/labels.js
+++ web/server/src/routes/labels.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/labels');
 const { errorHandler } = require('../common/errorHandler');
 
 router.post('', controller.add);
-router.get('', controller.getAll);
+router.get('', errorHandler(controller.getAll));
 router.get('/:num', controller.getOne);
 router.put('/:num', controller.update);
 router.delete('/:num', controller.delete);
