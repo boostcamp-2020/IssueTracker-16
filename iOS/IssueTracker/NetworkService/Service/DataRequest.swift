66d411252217af98d49661aebf84def59872b5f0
woongs <ccw1021@naver.com>
2020-10-29 18:11:16 +0900

    [iOS] feat: Request를 상속받은 Data, Download Request 생성
    - response 용도

---
+++ iOS/IssueTracker/NetworkService/Service/DataRequest.swift
@@ -0,0 +1,36 @@
+//
+//  DataRequest.swift
+//  NetworkService
+//
+//  Created by woong on 2020/10/29.
+//
+
+import Foundation
+
+open class DataRequest: Request {
+    
+    @discardableResult
+    func responseData(completionHandler: @escaping (Data?, NetworkResponse?) -> Void) -> Self {
+        guard let request = request else {
+            return self
+        }
+        
+        session.dataTask(with: request, completionHandler: { (data, response, error) in
+
+            guard error != nil else {
+                completionHandler(nil, .failed)
+                return
+            }
+
+            guard let data = data else {
+                completionHandler(nil, .noData)
+                return
+            }
+            
+            completionHandler(data, nil)
+        }).resume()
+        
+        return self
+    }
+    
+}
