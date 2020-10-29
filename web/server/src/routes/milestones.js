0a69c2ab4d3b6850b00c5373d984967080c64ba7
ahrimy <dlsdjdkfla@gmail.com>
2020-10-29 19:05:53 +0900

    [server] feat: milestone 등록 API error handler 적용
    - controller 내부 try..catch 코드 삭제
    - 전체 error handler 적용

--- web/server/src/routes/milestones.js
+++ web/server/src/routes/milestones.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const controller = require('../controllers/milestones');
 const { errorHandler } = require('../common/errorHandler');
 
-router.post('', controller.add);
+router.post('', errorHandler(controller.add));
 router.get('', controller.getAll);
 router.put('/:num', controller.update);
 router.delete('/:num', controller.delete);
