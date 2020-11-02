17de715991b3f21fdd65ec89d9cf2d2ab2b8ef18
kyungpyoda <qorghdl@naver.com>
2020-11-02 13:32:02 +0900

    [iOS] test: 색상-문자열 변환 단위 테스트 수정

--- iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
+++ iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
@@ -27,6 +27,20 @@ class IssueTrackerTests: XCTestCase {
         let red = UIColor(hex: redHex)
         XCTAssertEqual(red, UIColor.red)
         XCTAssertEqual(redHex, red?.hexString)
+        let blueHex = "0000FF"
+        let blue = UIColor(hex: blueHex)
+        XCTAssertEqual(blue, UIColor.blue)
+        XCTAssertEqual("#" + blueHex, blue?.hexString)
+        let blackHex = "000000"
+        let black = UIColor(hex: blackHex)
+        XCTAssertEqual(black, UIColor.init(red: 0, green: 0, blue: 0, alpha: 1))
+        XCTAssertEqual("#" + blackHex, black?.hexString)
+        let invalidHex1 = "#FF00000"
+        let invalid1 = UIColor(hex: invalidHex1)
+        XCTAssertNil(invalid1)
+        let invalidHex2 = "0000000"
+        let invalid2 = UIColor(hex: invalidHex2)
+        XCTAssertNil(invalid2)
     }
 
 }
