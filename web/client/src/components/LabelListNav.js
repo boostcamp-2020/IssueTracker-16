338c992ce8416a0899f182161a4d37b7ee5d14e9
ahrimy <dlsdjdkfla@gmail.com>
2020-11-04 00:29:00 +0900

    [client] feat: Label List Page를 구성하는 Nav와 메인 컴포넌트 추가
    - Label List Page를 구성하는 Nav 컴포넌트 추가
    - Label Header와 Label Body로 구성된 Label Liat 메인 컴포넌트 추가

---
+++ web/client/src/components/LabelListNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function LabelListNav() {
+  return (
+    <div>
+      <div>
+        <button>Labels</button>
+        <button>Milestones</button>
+      </div>
+      <button>New Issue</button>
+    </div>
+  );
+}
