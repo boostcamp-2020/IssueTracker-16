e29b1ecfaf30567ba1294c3f70251fb32cef89fa
ahrimy <dlsdjdkfla@gmail.com>
2020-11-03 17:20:02 +0900

    [client] feat: 헤더 컴포넌트 추가
    - header component 파일 생성
    - header component를 app component 에 추가

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Header from './component/Header';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Header />
+      Hello World!
+    </div>
+  );
 }
 export default App;
