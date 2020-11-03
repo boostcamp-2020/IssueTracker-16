bb3126beedabc7f0dbfce318dedf9a89c38af92f
kyungpyoda <qorghdl@naver.com>
2020-11-04 05:14:43 +0900

    [iOS] fix: 이슈 추가 버튼 콜렉션뷰 셀 위로 올라가도록 레이아웃 수정
    - 콜렉션뷰 셀과 레이아웃이 꼬이지 않게 위로 떠다니도록 수정
    - 콜렉션뷰 스크롤을 가장 밑으로 내리면 이슈추가버튼이 마지막 셀 위에 겹쳐있는 것을 방지하기 위해 section inset을 주었음.

--- iOS/IssueTracker/IssueTracker/ViewControllers/IssueViewController.swift
+++ iOS/IssueTracker/IssueTracker/ViewControllers/IssueViewController.swift
@@ -16,13 +16,17 @@ class IssueViewController: UIViewController {
     var interactor: IssueBusinessLogic?
     
     // MARK: - Views
+    @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         interactor = IssueInteractor()
-        navigationItem.searchController = searchController
+        configure()
+//        collectionView.contentSize.height = 100
+//        collectionView.collectionViewLayout.collectionViewContentSize.height.const = 100
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +38,28 @@ class IssueViewController: UIViewController {
     
 
     // MARK: - Initialize
+    
+    private func configure() {
+        navigationItem.searchController = searchController
+        let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout
+//        layout?.sectionFootersPinToVisibleBounds = true
+//        let btn = UIButton(type: .custom)
+//        
+//        btn.setBackgroundImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
+//        collectionView.addSubview(btn)
+//        btn.translatesAutoresizingMaskIntoConstraints = false
+//        let safeArea = view.safeAreaLayoutGuide
+//        NSLayoutConstraint.activate([
+//            btn.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+//            btn.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -20),
+//            btn.widthAnchor.constraint(equalToConstant: 50),
+//            btn.heightAnchor.constraint(equalToConstant: 50),
+//        ])
+//        btn.addTarget(self, action: #selector(test(sender:)), for: .touchUpInside)
+//        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+//            layout.sectionFootersPinToVisibleBounds
+//        }
+    }
 
     // MARK: - Methods
     
@@ -43,15 +69,22 @@ class IssueViewController: UIViewController {
     
     @IBAction func touchedAddIssueButton(_ sender: Any) {
         // TODO: - 이슈 추가화면 push
+        guard let indexPath = collectionView.indexPathsForVisibleItems.first else {
+            return
+        }
+        print(collectionView.indexPathsForVisibleItems)
+        print(collectionView.contentSize.height)
     }
 }
 
 extension IssueViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let c = collectionView.dequeueReusableCell(withReuseIdentifier: "testCell", for: indexPath)
+//        return c
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IssueListCollectionViewCell.identfier, for: indexPath) as? IssueListCollectionViewCell else { return UICollectionViewCell() }
         
         return cell
@@ -60,8 +93,29 @@ extension IssueViewController: UICollectionViewDataSource {
     
 }
 
+// MARK: - UICollectionView Delegate FlowLayout
+
 extension IssueViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return .init(width: view.bounds.width, height: 50)
     }
 }
+
+// MARK: - UICollectionView Delegate
+
+extension IssueViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+}
+
+/*
+ func addBottomSheetView() {
+     let bottomSheetVC = IssueBottomSheetViewController()
+     self.addChild(bottomSheetVC)
+     self.view.addSubview(bottomSheetVC.view)
+     bottomSheetVC.didMove(toParent: self)
+     let height = view.frame.height
+     let width = view.frame.width
+     bottomSheetVC.view.frame = CGRect(x: 0, y: view.frame.maxY, width: width, height: height)
+ }
+ */
