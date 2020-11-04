10825c111f21d211fc14968f2fc422a2bb4e2296
ahrimy <dlsdjdkfla@gmail.com>
2020-11-04 16:20:07 +0900

    [client] feat: Milestone Header, Body 컴포넌트 추가
    - Milestone 리스트를 구성하는 Header 와 Body 컴포넌트 구현

---
+++ web/client/src/components/MilestoneListBody.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MilestoneItem from './MilestoneItem';
+
+export default function MilestoneListBody() {
+  return (
+    <div className="milestone-list-body">
+      <MilestoneItem />
+      <MilestoneItem />
+    </div>
+  );
+}
---
+++ web/client/src/components/MilestoneListHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MilestoneListHeader() {
+  return (
+    <div className="milestone-list-header">
+      <span>[Open] </span>
+      <span>[Closed] </span>
+    </div>
+  );
+}
