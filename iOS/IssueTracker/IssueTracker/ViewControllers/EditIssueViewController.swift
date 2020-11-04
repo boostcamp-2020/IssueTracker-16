1fcf6da1a282cc80eb31c7a545f54ee53e0e7e7a
kyungpyoda <qorghdl@naver.com>
2020-11-05 02:59:42 +0900

    [iOS] feat: 이슈 추가 화면 UI 구현

---
+++ iOS/IssueTracker/IssueTracker/ViewControllers/EditIssueViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditIssueViewController.swift
+//  IssueTracker
+//
+//  Created by 홍경표 on 2020/11/05.
+//
+
+import UIKit
+
+class EditIssueViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
