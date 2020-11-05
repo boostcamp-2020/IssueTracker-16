ce207f47f757cc2afee4e128a3b92029a978a501
ahrimy <dlsdjdkfla@gmail.com>
2020-11-05 19:39:12 +0900

    [client] feat: 공통 컴포넌트 스타일 적용
    - 전체 폰트 설정
    - ListBody, ListHeader, ListItem 기본 스타일 적용

--- web/client/src/components/common/ListBody.js
+++ web/client/src/components/common/ListBody.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Ul = styled.ul``;
+const Ul = styled.ul`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default function ListBody({ children }) {
   return <Ul>{children}</Ul>;
--- web/client/src/components/common/ListHeader.js
+++ web/client/src/components/common/ListHeader.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Div = styled.div``;
+const Div = styled.div`
+  width: 80%;
+  padding: 2% 1%;
+  margin: 0 auto;
+  border: 0.1px solid #e0e0e0;
+  background-color: #c0c0c0;
+`;
 
 export default function ListHeader({ children }) {
   return <Div>{children}</Div>;
