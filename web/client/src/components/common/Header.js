fa0afa9e7903b96e69402542ba488f15a3de2bf2
skid901 <tlrudejr1993@naver.com>
2020-11-05 18:23:22 +0900

    [client] feat: 공통 컴포넌트 분리 및 추가
    - 헤더 컴포넌트 분리
    - 라벨 컴포넌트 분리
    - 링크 버튼 컴포넌트 추가
    - 폼 버튼 컴포넌트 추가
    - 라벨 및 마일스톤 페이지용 네비게이션 컴포넌트 추가
    - 리스트 헤더 컴포넌트 추가
    - 리스트 바디 컴포넌트 추가
    - 리스트 아이템 컴포넌트 추가

---
+++ web/client/src/components/common/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderElement = styled.header``;
+const H1 = styled.h1``;
+
+export default function Header() {
+  return (
+    <HeaderElement>
+      <H1>ISSUES</H1>
+    </HeaderElement>
+  );
+}
--- web/client/src/components/issueList/IssueItem.js
+++ web/client/src/components/issueList/IssueItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Label from '../labelList/Label';
+import Label from '../common/Label';
 
 export default function IssueItem({
   title,
