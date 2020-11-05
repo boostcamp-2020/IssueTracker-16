323e75b431c017f61224ddc3b26eb7947ad5abd7
seokju2ng <ehddnwnd@naver.com>
2020-11-05 17:24:46 +0900

    [client] feat: 공통 컴포넌트 Header 스타일 적용
    - Header의 스타일 적용
    - svg file을 따로 리액트 컴포넌트로 분리하고 import해서 사용

--- web/client/src/components/Header.js
+++ web/client/src/components/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import RepositoryLogo from '../statics/svg/repository';
+
+const HeaderElement = styled.header`
+  background-color: #24292e;
+  color: white;
+  padding: 21.5px 0;
+  font-size: 12pt;
+  font-weight: 600;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  div {
+    margin-left: 10px;
+  }
+`;
 
 export default function Header() {
   return (
-    <div className="header">
-      <h1 className="title">ISSUES</h1>
-    </div>
+    <HeaderElement>
+      <RepositoryLogo />
+      <div>ISSUES</div>
+    </HeaderElement>
   );
 }
