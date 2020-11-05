d176229c8c2090323037e059f7c8fc4735ee6b1b
ahrimy <dlsdjdkfla@gmail.com>
2020-11-05 20:48:27 +0900

    [client] feat: issue list 스타일 추가
    - issue list 메인 스타일 추가
    - issue List 의 header, body 스타일 추가
    - issue list page 전체 스타일 및 navigator 스타일 추가

--- web/client/src/components/issueList/IssueListNav.js
+++ web/client/src/components/issueList/IssueListNav.js
@@ -2,16 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 import SearchBar from './SearchBar';
-import LabelLogo from '../../statics/svg/label';
-import MilestoneLogo from '../../statics/svg/milestone';
+import LabelMilestoneNavigator from '../common/LabelMilestoneNavigator';
 
 const IssueListNavElement = styled.div`
+  width: 80%;
+  margin: 0 auto;
   display: flex;
   margin-top: 50px;
 `;
 
 const Buttons = styled.div`
   margin-left: auto;
+  display: flex;
 `;
 
 const GreenButton = styled.button`
@@ -29,44 +31,12 @@ const GreenButton = styled.button`
   }
 `;
 
-const TabButton = styled.a`
-  border: 1px solid lightgrey;
-  background-color: white;
-  font-size: 14px;
-  font-weight: 600;
-  padding: 7px 16px;
-
-  &:first-child {
-    border-top-left-radius: 6px;
-    border-bottom-left-radius: 6px;
-  }
-
-  &:nth-child(2) {
-    border-top-right-radius: 6px;
-    border-bottom-right-radius: 6px;
-    margin-left: -1px;
-    margin-right: 15px;
-  }
-
-  &:hover {
-    background-color: #fafbfc;
-    cursor: pointer;
-  }
-`;
-
 export default function IssueListNav() {
   return (
     <IssueListNavElement>
       <SearchBar />
       <Buttons>
-        <TabButton>
-          <LabelLogo />
-          Labels
-        </TabButton>
-        <TabButton>
-          <MilestoneLogo />
-          Milestones
-        </TabButton>
+        <LabelMilestoneNavigator />
         <GreenButton>New Issue</GreenButton>
       </Buttons>
     </IssueListNavElement>
