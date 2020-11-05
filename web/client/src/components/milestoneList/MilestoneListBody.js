4b33615af31e60199f7019c2b7d466d278a53f7e
ahrimy <dlsdjdkfla@gmail.com>
2020-11-05 19:40:28 +0900

    [client] feat: milestone 리스트 스타일 적용
    - 마일스톤 리스트  기본 레이아웃 설정
    - 마일스톤 progress bar 스타일 적용

--- web/client/src/components/milestoneList/MilestoneListBody.js
+++ web/client/src/components/milestoneList/MilestoneListBody.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import MilestoneItem from './MilestoneItem';
+import ListBody from '../common/ListBody';
 
 export default function MilestoneListBody() {
+  const [milestones, setMilestones] = useState([]);
+  useEffect(() => {
+    const getIssues = () => axios('/api/milestones');
+    getIssues().then(({ data }) => setMilestones(data));
+  }, []);
+
   return (
-    <div>
-      <MilestoneItem />
-      <MilestoneItem />
-    </div>
+    <ListBody>
+      {milestones.length &&
+        milestones.map(milestone => (
+          <MilestoneItem key={milestone.num} {...milestone} />
+        ))}
+    </ListBody>
   );
 }
