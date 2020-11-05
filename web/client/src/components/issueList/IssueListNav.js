0e08c865bf330eababbaa2b8b7542985cd29ab10
seokju2ng <ehddnwnd@naver.com>
2020-11-05 18:37:30 +0900

    [client] feat: issueList Nav 스타일 변경
    - issueListPage: 공통 width 900 지정
    - issueListNav: 우측 버튼 세개 스타일 이쁘게 지정

--- web/client/src/components/issueList/IssueListNav.js
+++ web/client/src/components/issueList/IssueListNav.js
@@ -1,15 +1,74 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import SearchBar from './SearchBar';
+import LabelLogo from '../../statics/svg/label';
+import MilestoneLogo from '../../statics/svg/milestone';
+
+const IssueListNavElement = styled.div`
+  display: flex;
+  margin-top: 50px;
+`;
+
+const Buttons = styled.div`
+  margin-left: auto;
+`;
+
+const GreenButton = styled.button`
+  padding: 7px 16px;
+  background-color: #2ea44f;
+  color: white;
+  border: 1px solid #268841;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+  border-radius: 6px;
+
+  &:hover {
+    background-color: #2a9847;
+  }
+`;
+
+const TabButton = styled.a`
+  border: 1px solid lightgrey;
+  background-color: white;
+  font-size: 14px;
+  font-weight: 600;
+  padding: 7px 16px;
+
+  &:first-child {
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
+  }
+
+  &:nth-child(2) {
+    border-top-right-radius: 6px;
+    border-bottom-right-radius: 6px;
+    margin-left: -1px;
+    margin-right: 15px;
+  }
+
+  &:hover {
+    background-color: #fafbfc;
+    cursor: pointer;
+  }
+`;
 
 export default function IssueListNav() {
   return (
-    <div>
+    <IssueListNavElement>
       <SearchBar />
-      <div>
-        <button>Labels</button>
-        <button>Milestones</button>
-        <button>New Issue</button>
-      </div>
-    </div>
+      <Buttons>
+        <TabButton>
+          <LabelLogo />
+          Labels
+        </TabButton>
+        <TabButton>
+          <MilestoneLogo />
+          Milestones
+        </TabButton>
+        <GreenButton>New Issue</GreenButton>
+      </Buttons>
+    </IssueListNavElement>
   );
 }
