58e941673b1f42f6210036985418ef125084b168
ahrimy <dlsdjdkfla@gmail.com>
2020-11-08 14:31:27 +0900

    [client] feat: Global 스타일 적용
    - styled-components 를 이용하여 global 스타일 적용
    - Globalstyle에 reset css 적용
    - reset css 를 위한 index.scss 제거

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
+import GlobalStyle from './GlobalStyle';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import IssueListPage from './pages/IssueListPage';
 import LabelListPage from './pages/LabelListPage';
@@ -9,6 +10,7 @@ import MilestoneListPage from './pages/MilestoneListPage';
 function App() {
   return (
     <>
+      <GlobalStyle />
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={IssueListPage} />
