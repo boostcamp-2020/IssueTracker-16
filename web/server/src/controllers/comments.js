68db71f57572e7f81114ed95b927a3959cc43d0b
seokju2ng <ehddnwnd@naver.com>
2020-11-09 17:22:53 +0900

    [server] feat: 코멘트 추가 API
    - 코멘트 추가 API 완성
    - 유저번호는 인증 정보에서 가져와야함

--- web/server/src/controllers/comments.js
+++ web/server/src/controllers/comments.js
@@ -2,7 +2,12 @@ const commentService = require('../services/comments');
 // const {} = require('../common/errorHandler');
 
 const commentController = {
-  add: async (req, res) => {},
+  add: async (req, res) => {
+    const { content, issueNum } = req.body;
+    const userNum = 1; // 인증 정보에서 userNum 값 빼오기
+    await commentService.add({ content, issueNum, userNum });
+    res.status(200).json({ success: true });
+  },
 
   update: async (req, res) => {},
 
--- web/server/src/services/comments.js
+++ web/server/src/services/comments.js
@@ -1,8 +1,8 @@
 const { sequelize, Comment, Issue, User } = require('../db/models');
 
 const commentService = {
-  add: async (req, res) => {},
-
+  add: async ({ content, userNum, issueNum }) =>
+    Comment.create({ content, user_num: userNum, issue_num: issueNum }),
   update: async (req, res) => {},
 
   delete: async (req, res) => {},
