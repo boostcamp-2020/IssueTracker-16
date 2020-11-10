9d39c319525542698af35f1d0637e749871ae84b
seokju2ng <ehddnwnd@naver.com>
2020-11-11 04:43:20 +0900

    [server] feat: 유저 모델에 프로필 이미지 컬럼 추가
    - 프로필 이미지 url 저장을 위한 컬럼 `imageUrl` 추가
    - 그에 따른 시더 데이터 추가

--- web/server/src/db/seeders/20201027083954-user.js
+++ web/server/src/db/seeders/20201027083954-user.js
@@ -4,14 +4,22 @@ module.exports = {
   up: async queryInterface =>
     queryInterface.bulkInsert('users', [
       {
-        id: 'user01',
-        password: 'user01',
-        name: 'user01',
+        id: 'seokju2ng',
+        password: 'password01',
+        name: 'Seokjung Kim',
+        image_url: 'https://avatars3.githubusercontent.com/u/43347250?s=40&v=4',
       },
       {
-        id: 'user02',
-        password: 'user02',
-        name: 'user02',
+        id: 'ahrimy',
+        password: 'password02',
+        name: 'Ahrim Yang',
+        image_url: 'https://avatars1.githubusercontent.com/u/26592306?s=40&v=4',
+      },
+      {
+        id: 'skid901',
+        password: 'password03',
+        name: 'Kyung Deok Si',
+        image_url: 'https://avatars2.githubusercontent.com/u/34153657?s=40&v=4',
       },
     ]),
   down: async queryInterface => queryInterface.bulkDelete('users'),
