748f0432cf2eb564a67f1d2c39ddbdc835920ee2
ahrimy <dlsdjdkfla@gmail.com>
2020-11-12 04:04:23 +0900

    [client] feat: Label list 에 input 컴포넌트 추가
    - Label List Page 에 Label Input 컴포넌트 추가
    - New Label 버튼클릭시 Input 컴포넌트 toggle 되도록 하는 이벤트 추가

--- web/client/src/pages/LabelListPage.js
+++ web/client/src/pages/LabelListPage.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Helmet from 'react-helmet';
 
 import ListPage from '../components/common/ListPage';
 import Header from '../components/common/Header';
 import LabelListNav from '../components/labelList/LabelListNav';
 import LabelList from '../components/labelList/LabelList';
+import LabelInput from '../components/labelList/LabelInput/LabelInput';
 
 export default function LabelListPage() {
+  const [state, setState] = useState({ isHidden: true });
+
   return (
     <>
       <Helmet>
@@ -14,7 +17,8 @@ export default function LabelListPage() {
       </Helmet>
       <Header />
       <ListPage>
-        <LabelListNav />
+        <LabelListNav {...{ ...state, setState }} />
+        {!state.isHidden && <LabelInput />}
         <LabelList />
       </ListPage>
     </>
