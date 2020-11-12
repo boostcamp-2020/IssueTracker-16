79932192cb032b4896689b4f067637ff184ba017
ahrimy <dlsdjdkfla@gmail.com>
2020-11-12 11:10:51 +0900

    [client] feat: label 삭제 버튼
    - label item 의 삭제버튼에 이벤트 추가

--- web/client/src/components/labelList/LabelItem.js
+++ web/client/src/components/labelList/LabelItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 import ListItem from '../common/ListItem';
@@ -6,15 +7,18 @@ import Label from '../common/Label';
 
 const Description = styled.div`
   width: 55%;
+  margin: auto 0;
 `;
 
 const LabelView = styled.div`
   width: 25%;
+  margin: auto 0;
 `;
 
 const Buttons = styled.div`
   text-align: right;
   width: 20%;
+  margin: auto 0;
 `;
 
 const Button = styled.button`
@@ -28,6 +32,21 @@ const Button = styled.button`
 
 export default function LabelItem({ num, name, color, description }) {
   const isLabelList = true;
+  const deleteLabel = num => axios.delete(`/api/labels/${num}`);
+  const handleDeleteClick = event => {
+    event.preventDefault();
+    if (
+      window.confirm(
+        'Are you sure? Deleting a label will remove it from all issues and pull requests.',
+      )
+    ) {
+      deleteLabel(num)
+        .then(({ data }) => {
+          if (data.success) window.location = '/labels';
+        })
+        .catch(({ response }) => alert(response.data.message));
+    }
+  };
   return (
     <ListItem>
       <LabelView>
@@ -36,7 +55,7 @@ export default function LabelItem({ num, name, color, description }) {
       <Description>{description}</Description>
       <Buttons>
         <Button>Edit</Button>
-        <Button>Delete</Button>
+        <Button onClick={handleDeleteClick}>Delete</Button>
       </Buttons>
     </ListItem>
   );
