d6806821c309446826766ccbab766c75db79199a
skid901 <tlrudejr1993@naver.com>
2020-11-13 03:45:46 +0900

    [client] refactor: 쿠키 삭제 함수 수정

--- web/client/src/utils/cookies.js
+++ web/client/src/utils/cookies.js
@@ -9,7 +9,7 @@ const cookies = {
     document.cookie = `${key}=${val}`;
   },
   deleteCookie(key) {
-    this.setCookie(key, '');
+    this.setCookie(key, '; expires=Thu, 01 Jan 1999 00:00:10 GMT;');
   },
 };
 
