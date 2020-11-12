6d367adcb5e0bb3db4203eaa16809a9c737a57f6
ahrimy <dlsdjdkfla@gmail.com>
2020-11-12 10:39:20 +0900

    [client] fix: isValidColor와 isLightColor 판단기준 통일
    - isValidColor에서는 6자리와 3자리를 유효하다고 판단하지만 isLightColor에서는 6자리만 유효하다고 판단하기 때문에 3자리일때 글씨 색이 변경되지 않는 버그 발생
    - 6자리만 유효하도록 변경

--- web/client/src/utils/isValidColor.js
+++ web/client/src/utils/isValidColor.js
@@ -1,4 +1,3 @@
-const isValidColor = color =>
-  /(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{3}$)/.test(color);
+const isValidColor = color => /(^#[0-9A-Fa-f]{6}$)/.test(color);
 
 export default isValidColor;
